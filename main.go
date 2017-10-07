08cc030ea0f88ed846be3de45bae3aa584a8d9c9
NoahOrberg <noah.orberg.9999@gmail.com>
2017-10-07 16:57:53 +0900

    implementd file upload

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"github.com/NoahOrberg/gilbert/gist"
 	"github.com/neovim/go-client/nvim"
 	"github.com/neovim/go-client/nvim/plugin"
 )
 
 func gilbertUp(v *nvim.Nvim, args []string) error {
-	lines := [][]byte{[]byte("gilbert"), []byte("nvim")}
-	buf, _ := v.CurrentBuffer()
-	return v.SetBufferLines(buf, 0, -1, true, lines)
+	buf, err := v.CurrentBuffer()
+	if err != nil {
+		return err
+	}
+	filename, err := v.BufferName(buf)
+	if err != nil {
+		return err
+	}
+	err = gist.PostToGist("", filename)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
