65016c067b4f661e28655678a2c887c778c19037
NoahOrberg <noah.orberg.9999@gmail.com>
2017-10-08 02:03:24 +0900

    fix

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 )
 
 func main() {
+	g := &command.Gilbert{}
 	plugin.Main(func(p *plugin.Plugin) error {
-		p.HandleCommand(&plugin.CommandOptions{Name: "GiUpload", NArgs: "*"}, command.GilbertUpload)
+		p.HandleCommand(&plugin.CommandOptions{Name: "GiUpload", NArgs: "?"}, g.GilbertUpload)
 		return nil
 	})
 }
