ad84827625cf77087debaa36aa42dbaceaa78137
NoahOrberg <noah.orberg.9999@gmail.com>
2017-10-08 01:04:07 +0900

    refactor

--- main.go
+++ main.go
@@ -1,59 +1,13 @@
 package main
 
 import (
-	"github.com/NoahOrberg/gilbert/gist"
-	"github.com/neovim/go-client/nvim"
+	"github.com/NoahOrberg/gilbert.nvim/src/command"
 	"github.com/neovim/go-client/nvim/plugin"
 )
 
-func gilbertUpload(v *nvim.Nvim, args []string) error {
-	buf, err := v.CurrentBuffer()
-	if err != nil {
-		return err
-	}
-
-	filename, err := v.BufferName(buf)
-	if err != nil {
-		return err
-	}
-
-	var url string
-	if filename == "" {
-		if len(args) > 0 {
-			filename = args[0]
-		} else {
-			filename = "NoName"
-		}
-		lines, err := v.BufferLines(buf, 0, -1, true)
-		if err != nil {
-			return err
-		}
-
-		var content string
-		for _, c := range lines {
-			content += string(c)
-			content += "\n"
-		}
-
-		url, err = gist.PostToGistByContent("", filename, content)
-		if err != nil {
-			return err
-		}
-	} else {
-		url, err = gist.PostToGistByFile("", filename, false)
-		if err != nil {
-			return err
-		}
-	}
-
-	v.WriteOut(url)
-
-	return nil
-}
-
 func main() {
 	plugin.Main(func(p *plugin.Plugin) error {
-		p.HandleCommand(&plugin.CommandOptions{Name: "GiUpload", NArgs: "*"}, gilbertUpload)
+		p.HandleCommand(&plugin.CommandOptions{Name: "GiUpload", NArgs: "*"}, command.gilbertUpload)
 		return nil
 	})
 }
