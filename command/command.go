b530d7104c03c039cc7804c2868dd7dceffdc23f
NoahOrberg <noah.orberg.9999@gmail.com>
2017-10-09 00:25:21 +0900

    add PATCH method

--- command/command.go
+++ command/command.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"encoding/json"
 	"errors"
 	"strings"
 
@@ -8,7 +9,59 @@ import (
 	"github.com/neovim/go-client/nvim"
 )
 
+const noName = "NoName"
+
 type Gilbert struct {
+}
+
+func (g *Gilbert) GilbertPatch(v *nvim.Nvim, args []string) error {
+	buf, err := v.CurrentBuffer()
+	if err != nil {
+		return err
+	}
+
+	filename, err := v.BufferName(buf)
+	if err != nil {
+		return err
+	}
+	temp := strings.Split(filename, "/")
+	filename = temp[len(temp)-1]
+	if filename == "" {
+		filename = noName
+	}
+
+	lines, err := v.BufferLines(buf, 0, -1, true)
+	if err != nil {
+		return err
+	}
+
+	var content string
+	for _, c := range lines {
+		content += string(c)
+		content += "\n"
+	}
+
+	if len(args) != 1 {
+		return errors.New("invalid argument(need only one)")
+	}
+	id := args[0]
+	gi := gist.Gist{
+		Files: map[string]gist.File{
+			filename: gist.File{
+				Content: content,
+			},
+		},
+	}
+	hoge, err := json.Marshal(gi)
+	v.Command("echom '" + filename + "'")
+	v.Command("echom '" + string(hoge) + "'")
+	err = gist.PatchGist(id, gi)
+	if err != nil {
+		v.Command("echo '" + err.Error() + "'")
+		return err
+	}
+
+	return nil
 }
 
 func (g *Gilbert) GilbertLoad(v *nvim.Nvim, args []string) error {
@@ -61,7 +114,7 @@ func (g *Gilbert) GilbertUpload(v *nvim.Nvim, args []string) error {
 		if len(args) > 0 {
 			filename = args[0]
 		} else {
-			filename = "NoName"
+			filename = noName
 		}
 		lines, err := v.BufferLines(buf, 0, -1, true)
 		if err != nil {
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	plugin.Main(func(p *plugin.Plugin) error {
 		p.HandleCommand(&plugin.CommandOptions{Name: "GiUpload", NArgs: "?"}, g.GilbertUpload)
 		p.HandleCommand(&plugin.CommandOptions{Name: "GiLoad", NArgs: "1"}, g.GilbertLoad)
+		p.HandleCommand(&plugin.CommandOptions{Name: "GiPatch", NArgs: "1"}, g.GilbertPatch)
 		return nil
 	})
 }
