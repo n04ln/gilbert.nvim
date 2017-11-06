4a9fb2b9089d30917b7cbe1aefcf049d7cd48ad7
NoahOrberg <noah.orberg.9999@gmail.com>
2017-11-06 13:22:12 +0900

    refactor command/util.go

--- command/util.go
+++ command/util.go
@@ -2,20 +2,65 @@ package command
 
 import (
 	"errors"
+	"fmt"
 	"strconv"
 
 	"github.com/NoahOrberg/nvim-go-util/util"
 	"github.com/neovim/go-client/nvim"
 )
 
-const noName = "NoName"
+type Scoop int
+
+const (
+	GlobalScoop           = iota + 1
+	ScriptScoop
+	LocalScoop
+	BufferScoop
+	WindowScoop
+	BuiltinFuncScoop
+)
+
+func (s Scoop) String() string {
+	switch s {
+	case GlobalScoop:
+		return "g:"
+	case ScriptScoop:
+		return "s:"
+	case LocalScoop:
+		return "l:"
+	case BufferScoop:
+		return "b:"
+	case WindowScoop:
+		return "w:"
+	case BuiltinFuncScoop:
+		return "v:"
+	}
+	return ""
+}
+
+const (
+	noName = "NoName"
+	// vimscript variable
+	// GlobalScoop (prefix is `g:`)
+	allowOpenByBrowser       = "gilbert#allow_open_by_browser"
+	shouldCopyURLToClipBoard = "gilbert#should_copy_url_to_clipboard"
+	bufferAndGistIDInfo      = "gilbert#buffer_and_gist_id_info"
+	isLoadedByGiLoad         = "gilbert#is_loaded_by_giload"
+	// the format of func, "****()"
+	funcClearUndo = "gilbert#clear_undo()"
+	//Error
+)
+
+var ErrUnexpectedType = func(t string) error {
+	return fmt.Errorf("unexpected type : %s", t)
+}
 
 type Gilbert struct{}
 
 // g:gilbert#allow_open_by_browser をチェックし、1ならブラウザで開く
 func checkAndOpenGist(v *nvim.Nvim, url string) error {
 	var isAllow int
-	if v.Var("gilbert#allow_open_by_browser", &isAllow); isAllow == 1 {
+	if v.Var(allowOpenByBrowser, &isAllow); isAllow == 1 {
 		if err := util.Exec(v, "open "+url); err != nil {
 			return err
 		}
@@ -26,7 +71,7 @@ func checkAndOpenGist(v *nvim.Nvim, url string) error {
 // g:gilbert#should_copy_url_to_clipboard をチェック
 func checkAndCopyGistURL(v *nvim.Nvim, url string) error {
 	var isAllow int
-	if v.Var("gilbert#should_copy_url_to_clipboard", &isAllow); isAllow == 1 {
+	if v.Var(shouldCopyURLToClipBoard, &isAllow); isAllow == 1 {
 		if err := util.Exec(v, "echo '"+url+"' | pbcopy"); err != nil {
 			return err
 		}
@@ -37,7 +82,7 @@ func checkAndCopyGistURL(v *nvim.Nvim, url string) error {
 // gist_idの情報を得る
 func getGistIDFromBufferID(v *nvim.Nvim, buf nvim.Buffer) (string, error) {
 	var bufferMap map[string]string
-	v.Var("gilbert#buffer_and_gist_id_info", &bufferMap)
+	v.Var(bufferAndGistIDInfo, &bufferMap)
 	res, ok := bufferMap[strconv.Itoa(int(buf))]
 	if !ok {
 		return "", errors.New("this buffer is not gist")
@@ -48,12 +93,12 @@ func getGistIDFromBufferID(v *nvim.Nvim, buf nvim.Buffer) (string, error) {
 // 指定gist_idのバッファと、vimscript変数からすべて消す
 func deleteBuffersOfGistID(v *nvim.Nvim, gistID string) error {
 	var bufferMap map[string]string
-	v.Var("gilbert#buffer_and_gist_id_info", &bufferMap)
+	v.Var(bufferAndGistIDInfo, &bufferMap)
 	b := v.NewBatch()
 	for key, value := range bufferMap {
 		if value == gistID {
 			b.Command("bw! " + key)
-			b.Command("call remove(g:gilbert#buffer_and_gist_id_info, '" + key + "')")
+			removeKeyOfMapByBatch(b, GlobalScoop, bufferAndGistIDInfo, key)
 		}
 	}
 	if err := b.Execute(); err != nil {
@@ -66,7 +111,7 @@ func getBufferIDsFromGistID(v *nvim.Nvim, gistID string) ([]nvim.Buffer, error) 
 	bs := []nvim.Buffer{}
 
 	var bufferMap map[string]string
-	v.Var("gilbert#buffer_and_gist_id_info", &bufferMap)
+	v.Var(bufferAndGistIDInfo, &bufferMap)
 	for k, v := range bufferMap {
 		if v == gistID {
 			b, err := strconv.Atoi(k)
@@ -86,36 +131,70 @@ func getBufferIDsFromGistID(v *nvim.Nvim, gistID string) ([]nvim.Buffer, error) 
 // gist_idの情報を追加
 func setGistIDFromBufferID(v *nvim.Nvim, buf nvim.Buffer, gistID string) error {
 	var bufferMap map[string]string
-	if err := v.Var("gilbert#buffer_and_gist_id_info", &bufferMap); err != nil {
+	if err := v.Var(bufferAndGistIDInfo, &bufferMap); err != nil {
 		return err
 	}
 	// bufferMap[int(buf)] = gistID
 	b := strconv.Itoa(int(buf))
 	// return v.SetVar("gilbert#buffer_and_gist_id_info", bufferMap)
-	return v.Command("let g:gilbert#buffer_and_gist_id_info['" + b + "']='" + gistID + "'")
+	return setValueOfMap(v, GlobalScoop, bufferAndGistIDInfo, b, gistID)
 }
 
 // Loadしたものかどうかを入れておく
 func setGistIDToGiLoaded(v *nvim.Nvim, gistID string) error {
 	var flagMap map[string]int
-	if err := v.Var("gilbert#is_loaded_by_giload", &flagMap); err != nil {
+	if err := v.Var(isLoadedByGiLoad, &flagMap); err != nil {
 		return err
 	}
-	return v.Command("let g:gilbert#is_loaded_by_giload['" + gistID + "']=1")
+	return setValueOfMap(v, GlobalScoop, isLoadedByGiLoad, gistID, 1)
 }
 
 // mapにして返す
 func getGistIDByGiLoaded(v *nvim.Nvim) (map[string]int, error) {
 	var flagMap map[string]int
-	if err := v.Var("gilbert#is_loaded_by_giload", &flagMap); err != nil {
+	if err := v.Var(isLoadedByGiLoad, &flagMap); err != nil {
 		return map[string]int{}, err
 	}
 	return flagMap, nil
 }
 
+// setValueOfMap
+// scoop => g, s, l, and so on
+// TODO: error handling of unexpected scoop
+func setValueOfMap(v *nvim.Nvim, s Scoop, variable, index string, value interface{}) error {
+	var val string
+	switch value.(type) {
+	case int:
+		val = fmt.Sprintf("%d", value.(int))
+	case string:
+		val = fmt.Sprintf("'%s'", value.(string))
+	case bool:
+		if value.(bool) {
+			val = "1"
+		} else {
+			val = "0"
+		}
+	default:
+		valType := fmt.Sprintf("%T", value)
+		return ErrUnexpectedType(valType) // TODO: should return TYPE;;
+	}
+	return v.Command("let " + s.String() + variable + "['" + index + "']=" + val)
+}
+
+// removeKeyOfMap
+// scoop => g, s, l, and so on
+func removeKeyOfMapByBatch(b *nvim.Batch, s Scoop, variable, key string) {
+	b.Command("call remove(" + s.String() + variable + ", '" + key + "')")
+}
+
 // delete history
 func clearUndo(v *nvim.Nvim) error {
-	return v.Command("call gilbert#clear_undo()")
+	return callFunc(v, funcClearUndo)
+}
+
+// call function
+func callFunc(v *nvim.Nvim, f string) error {
+	return v.Command("call " + f)
 }
 
 // save
